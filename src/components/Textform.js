b002ca6f9e4367095526a8238139a6010d8b6cf4
Md. Mazharul Islam Antor <antormazharul@gmail.com>
2022-12-13 14:51:59 +0600

    Dark Mode

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -11,6 +11,21 @@ export default function Textform(props) {
     setText(newText);
   };
 
+  const clearTextArea = () => {
+    let newText = "";
+    setText(newText);
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[  ]+/);
+    setText(newText.join(" "));
+  };
+  const handleCopy = () => {
+    var newText = document.getElementById("myBox");
+    newText.select();
+    navigator.clipboard.writeText(newText.value);
+  };
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
@@ -18,31 +33,52 @@ export default function Textform(props) {
   const [text, setText] = useState("");
   return (
     <>
-    <div className="container">
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpperClick}>
+          Convert to uppercase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={handleLowerClick}>
+          Convert to lowercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={clearTextArea}>
+          Clear
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+          Copy
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+          Remove Extra Spaces
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpperClick}>
-        Convert to uppercase
-      </button>
-      <button className="btn btn-primary mx-3" onClick={handleLowerClick}>
-        Convert to lowercase
-      </button>
-    </div>
-    <div className="container my-3">
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words, {text.length} characters</p>
-        <p>{0.008*text.split(" ").length} minutes to read</p>
+        <p>
+          {text.split(" ").length} words, {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} minutes to read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
-    </div>
+        <p>{text.length > 0 ? text : "Enter something in the textbox to preview here"}</p>
+      </div>
     </>
   );
 }
